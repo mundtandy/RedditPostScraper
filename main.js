27c720933f653c560adfd8d969eaf4e0004dcf43
mundtandy <mundtandy@ecs.vuw.ac.nz>
2017-11-07 17:48:57 +1300

    separated browser detection and website detection

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+var browser;
+
+//add JS functionality to button 'Submit'
 document.addEventListener('DOMContentLoaded', function() {
 	document.getElementById("submit").addEventListener("click", submitHandler);
 });
@@ -5,33 +8,64 @@ document.addEventListener('DOMContentLoaded', function() {
 // Detects Browser
 function detectBrowser() { 
 	if((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1 ) {
-		setText('browser', 'Opera');
-		browser = 'Opera';
+		return 'Opera'
 	} else if(navigator.userAgent.indexOf("Chrome") != -1 ) {
-		setText('browser', 'Chrome');
-		//detect if using reddit
-		chrome.tabs.getSelected(null, function(tab) {
-			var tabUrl = new URL(tab.url).hostname
-			var tabHost = tabUrl
-			
-			setText('website', (tabHost.match('www.reddit.com') ? 'yes!' : 'no!'))
-		});
-		
+		return 'Chrome'
 	} else if(navigator.userAgent.indexOf("Safari") != -1) {
-		setText('browser', 'Safari');
-		
+		return 'Safari'
 	} else if(navigator.userAgent.indexOf("Firefox") != -1 ) {
-		setText('browser', 'Firefox');
-		
+		return 'Firefox'
 	} else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) /*IF IE > 10*/ {
-		setText('browser', 'Edge');
-		
+		return 'Edge'
 	} else {
-		setText('browser', 'Invalid browser');
-		setText('website', 'Unknown');
+		return 'Invalid browser'
 	}
 }
 
+//detects hostname of current website URL, using browser specific function
+function onReddit(){
+	var tx = 'website'
+	switch(browser) {
+		case 'Opera' :
+			//toAdd Opera version
+			break;
+		case 'Chrome' :
+			chrome.tabs.getSelected(null, function(tab) {
+				var urlHost = new URL(tab.url).hostname
+				setText(tx, (urlHost.match('www.reddit.com') ? 'yes' : 'no'))
+			});
+			break;
+		case 'Safari' :
+			//toAdd Safari version
+			break;
+		case 'Firefox' :
+			//toAdd Firefox version
+			break;
+		case 'Edge' :
+			//toAdd Edge version
+			break;
+		default :
+			setText(tx, 'Unknown')
+	}
+}
+
+//sets user area based on previous login details
+function setUserArea() {
+	//if stored user exists
+		//setExists()
+	//else
+		setFresh()
+}
+
+function setFresh() {
+	document.getElementById("userArea").innerHTML="Username:<br>"
+	+"<input type=\"text\" name=\"username\"><br>"
+	+"Password:<br>"
+	+"<input type=\"password\" name=\"password\">"
+	+"<button id=\"submit\">Submit</button>"; 
+}
+
+//submit login details
 function submitHandler() {
 	alert("Hello");
 }
@@ -41,5 +75,7 @@ function setText(ele, toChange){
 	document.getElementById(ele).innerHTML += toChange;
 }
 
-detectBrowser();
-onReddit();
+browser = detectBrowser()
+setText('browser', browser)
+onReddit()
+setUserArea()
