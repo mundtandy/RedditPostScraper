25948ef4c07c1ef6749777321612fbe234670ad2
mundtandy <mundtandy@ecs.vuw.ac.nz>
2017-11-14 22:59:34 +1300

    base url parsing functionality done

--- event.js
+++ event.js
@@ -6,7 +6,21 @@ chrome.contextMenus.create({
 	"title": "Parse Reddit Post",
 	"contexts": ["link"],
 	"targetUrlPatterns": showForPages,
-	"onclick" : function(e) {
-		alert("hello");
+	"onclick" : function(obj) {
+		parseUrl(obj.linkUrl);
+		//apiCall(obj);
 	}
 });
+
+//parse url
+function parseUrl(url){
+	var parsed = url.split("/");
+	var sub = parsed[4];
+	var id = "t3_"+parsed[6];
+	alert("sub: "+sub+"\nid: "+id);
+}
+
+//calls Reddit Api
+function apiCall(obj) {
+	alert(obj.linkUrl);
+}
